cd96586a3d9f62db25c960a203ec3a2a9497947b
Max Burke <maxburke@gmail.com>
2010-09-26 15:02:04 -0700

    More work done on the shader reader. Fleshed out the parser fns but they still need to be implemented.

--- ShaderReader/ShaderReader/Program.cs
+++ ShaderReader/ShaderReader/Program.cs
@@ -9,7 +9,15 @@ namespace ShaderReader
 {
     class Shader
     {
+        public enum CullMode
+        {
+            Front,
+            Back,
+            None
+        }
+
         string mName;
+        CullMode mCullMode = CullMode.Front;
 
         public Shader(string name)
         {
@@ -25,16 +33,127 @@ namespace ShaderReader
 
     class ShaderParser
     {
+        class TokenParser
+        {
+            public readonly string Token;
+            public delegate void ParseFunction();
+            public readonly ParseFunction Function;
+
+            public TokenParser(string token, ParseFunction function)
+            {
+                Token = token;
+                Function = function;
+            }
+        }
+
+        public void DefaultParseFunction()
+        {
+        }
+
+        TokenParser[] mGeneralTokenParsers;
+        TokenParser[] mShaderStageTokenParsers;
+
+        void SkyParms()
+        {
+            string farBox = NextToken();
+            string cloudHeight = NextToken();
+            string nearbox = NextToken();
+        }
+
+        void Cull() { }
+        void DeformVertexes() { }
+        void FogParms() { }
+        void Nopicmip() { }
+        void Nomipmap() { }
+        void PolygonOffset() { }
+        void Portal() { }
+        void Sort() { }
+        void TessSize() { }
+        void Q3mapBackshader() { }
+        void Q3mapGlobalTexture() { }
+        void Q3mapSun() { }
+        void Q3mapSurfaceLight() { }
+        void Q3mapLightImage() { }
+        void Q3mapLightSubdivide() { }
+        void SurfaceParm() { }
+        void QerEditorImage() { }
+        void QerNoCarve() { }
+        void QerTrans() { }
+
+
+        void Map() { }
+        void ClampMap() { }
+        void AnimMap() { }
+        void BlendFunc() { }
+        void RgbGen() { }
+        void AlphaGen() { }
+        void TcGen() { }
+        void TcMod() { }
+        void DepthFunc() { }
+        void DepthWrite() { }
+        void AlphaFunc() { }
+
+        void InitializeTokenParsers()
+        {
+            mGeneralTokenParsers = new TokenParser[]
+            {
+                new TokenParser("skyparms", SkyParms),
+                new TokenParser("cull", Cull),
+                new TokenParser("deformvertexes", DeformVertexes),
+                new TokenParser("fogparms", FogParms),
+                new TokenParser("nopicmip", Nopicmip),
+                new TokenParser("nomipmap", Nomipmap),
+                new TokenParser("polygonoffset", PolygonOffset),
+                new TokenParser("portal", Portal),
+                new TokenParser("sort", Sort),
+                new TokenParser("tesssize", TessSize),
+                new TokenParser("q3map_backshader", Q3mapBackshader),
+                new TokenParser("q3map_globaltexture", Q3mapGlobalTexture),
+                new TokenParser("q3map_sun", Q3mapSun),
+                new TokenParser("q3map_surfacelight", Q3mapSurfaceLight),
+                new TokenParser("q3map_lightimage", Q3mapLightImage),
+                new TokenParser("q3map_lightsubdivide", Q3mapLightSubdivide),
+                new TokenParser("surfaceparm", SurfaceParm),
+                new TokenParser("qer_editorimage", QerEditorImage),
+                new TokenParser("qer_nocarve", QerNoCarve),
+                new TokenParser("qer_trans", QerTrans)
+            };
+
+            mShaderStageTokenParsers = new TokenParser[] 
+            {
+                new TokenParser("map", Map),
+                new TokenParser("clampmap", ClampMap),
+                new TokenParser("animmap", AnimMap),
+                new TokenParser("blendfunc", BlendFunc),
+                new TokenParser("rgbgen", RgbGen),
+                new TokenParser("alphagen", AlphaGen),
+                new TokenParser("tcgen", TcGen),
+                new TokenParser("tcmod", TcMod),
+                new TokenParser("depthfunc", DepthFunc),
+                new TokenParser("depthwrite", DepthWrite),
+                new TokenParser("alphafunc", AlphaFunc)
+            };
+
+            Array.Sort(mGeneralTokenParsers, (a, b) => { return a.Token.CompareTo(b.Token); });
+            Array.Sort(mShaderStageTokenParsers, (a, b) => { return a.Token.CompareTo(b.Token); });
+        }
+
+
+        Stack<string> mPushedTokens = new Stack<string>();
+        Shader mShader;
         string mContent;
+        string mCurrentEffect;
         int I;
         int E;
 
         public ShaderParser(string content)
-        {
+        {   
             I = 0;
             E = content.Length;
             mContent = content;
-            Parse();
+            InitializeTokenParsers();
+
+            ParseAllShaders();
         }
 
         void ConsumeWhitespace()
@@ -59,6 +178,9 @@ namespace ShaderReader
 
         string NextToken()
         {
+            if (mPushedTokens.Count != 0)
+                return mPushedTokens.Pop();
+
             if (I == E)
                 return null;
 
@@ -79,38 +201,90 @@ namespace ShaderReader
             return mContent.Substring(start, end - start);
         }
 
-        void Parse()
+        string NextTokenLowerCase()
+        {
+            string s = NextToken();
+            if (s == null)
+                return s;
+            return s.ToLower();
+        }
+
+        void PushToken(string s)
+        {
+            mPushedTokens.Push(s);
+        }
+
+        void ParseAllShaders()
+        {
+            while (ParseShader())
+            {
+                // do something with mShader
+                mShader = null;
+            }
+        }
+
+        string Expect(string str)
+        {
+            string token = NextToken();
+            if (token != str)
+            {
+                throw new Exception(
+                    string.Format("Shit's malformed, just sayin'. While parsing effect '{0}', I expected '{1}' and got '{2}'",
+                        mCurrentEffect, str, token));
+            }
+            return token;
+        }
+
+        TokenParser FindParser(TokenParser[] parsers, string token)
+        {
+            return Array.Find<TokenParser>(parsers, x => x.Token == token);
+        }
+
+        void ParseGeneralShaderCode()
         {
             for (; ; )
             {
-                int nestLevel = 0;
-                string effectName = NextToken();
-                if (effectName == null)
-                    return;
+                string token = NextTokenLowerCase();
+                TokenParser tokenParser = FindParser(mGeneralTokenParsers, token);
 
-                Shader S = new Shader(effectName);
+                if (tokenParser == null)
+                    throw new Exception(string.Format("shit's broken. failing on token '{0}'", token));
 
-                for (; ; )
+                tokenParser.Function();
+
+                if (token == "{")
                 {
-                    string token = NextToken();
-
-                    if (token == "{")
-                    {
-                        ++nestLevel;
-                        continue;
-                    }
-
-                    if (token == "}")
-                    {
-                        --nestLevel;
-
-                        if (nestLevel == 0)
-                            break;
-                    }
+                    PushToken(token);
+                    return;
                 }
-
-                Console.WriteLine(S);
             }
+        }
+
+        void ParseShaderStages()
+        {
+            string t = NextToken();
+            if (t != "{")
+                return;
+        }
+
+        bool ParseShader()
+        {
+            mCurrentEffect = NextToken();
+            if (mCurrentEffect == null)
+                return false;
+
+            mShader = new Shader(mCurrentEffect);
+
+            Expect("{");
+
+            ParseGeneralShaderCode();
+            ParseShaderStages();
+
+            Expect("}");
+
+            Console.WriteLine(mShader);
+
+            return true;
         }
     }
 
