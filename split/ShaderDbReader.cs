821b96bd2529128c5f0b42019bb5220ee6a83249
Max Burke <maxburke@gmail.com>
2011-01-09 19:07:05 -0800

    Added ShaderDbReader and updated project files for new shader database.

---
+++ split/ShaderDbReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Split.Pipeline;
+using Slim;
+
+namespace Split
+{
+    class ShaderDbReader : ContentTypeReader<ShaderDb>
+    {
+        protected override ShaderDb Read(ContentReader input, ShaderDb existingInstance)
+        {
+            ShaderDb shaderDb = new ShaderDb();
+            shaderDb.Parse(input);
+            return shaderDb;
+        }
+    }
+}
